976dfaf778795c88ac856d2fb65e61a6dc3001a9
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-05-12 17:21:01 -0400

    Delete SC-DFE directory

--- SC-DFE/ZP_zeropaddle.m
+++
@@ -1,8 +0,0 @@
-function Symbols2=ZP_zeropaddle(Symbols,Lzp,N,Block_Num)
-%At each encoded symbol block, Lzp zero symbols are appended, for OFDM and
-%Zp-only to have equal symbol rate Lzp=Lcp. 
-Symbols2=zeros(N+Lzp,1,Block_Num);
-for a=1:Block_Num
-   Symbols2(:,:,a)=[Symbols(:,:,a);zeros(Lzp,1)];
-end
-end
