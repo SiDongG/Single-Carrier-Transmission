8ab85eb0974c5eb8a79cd0a5d9152ca45aae82aa
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-05-12 17:20:51 -0400

    Add files via upload

---
+++ SC-FDE/ZP_init copy.m
@@ -0,0 +1,21 @@
+function [Bits,Symbols]=ZP_init(Block_Num,N) 
+%Block_Num is the number of blocks and N is the symbol length of each block
+Bits=randi(0:1,[1,Block_Num*N*2]); %L bits in the bit-stream 
+count=1;
+Symbols=zeros(N,1,Block_Num); %Organize symbols into blocks
+while count<length(Bits)
+        if (Bits(count)==0 && Bits(count+1)==0)
+            Symbols((count+1)/2)=(1+1i)/sqrt(2);
+        end
+        if (Bits(count)==0 && Bits(count+1)==1)
+            Symbols((count+1)/2)=(-1+1i)/sqrt(2);
+        end
+        if (Bits(count)==1 && Bits(count+1)==1)
+            Symbols((count+1)/2)=(-1-1i)/sqrt(2);
+        end
+        if (Bits(count)==1 && Bits(count+1)==0)
+            Symbols((count+1)/2)=(1-1i)/sqrt(2);
+        end
+        count=count+2;
+end
+end
